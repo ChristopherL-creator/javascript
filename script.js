f0b3abd6e9c08c392e480476e108545db2d5c1b3
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-04-20 10:19:50 +0200

    ini

---
+++ script.js
@@ -0,0 +1,75 @@
+// STRINGHE
+console.log('hello world'); 
+
+// alert("hello world"); 
+console.log("hello cinzia"); 
+
+console.log('7');  
+
+console.log("andrea" + "è" + "in" + "ritardo", "attacca tutti gli elementi");
+
+// NUMBER  
+console.log(7); 
+
+console.log('somma', 10+5); 
+
+console.log('sottrazione', 10-5); 
+
+console.log('moltiplicazione', 10*5); 
+
+console.log('divisione', 10/5); 
+
+console.log("potenza", 5 ** 2); 
+
+console.log("modulo", 5 % 2, "da' il resto della divisione"); 
+
+console.log("Math", Math.round(3.56), "round arrotonda"); 
+
+// complicazioni 
+console.log("ciao" + 7, "somma stringhe e numeri a prescindere"); 
+
+console.log("7" + 7); 
+
+// boolean 
+console.log(true); 
+
+console.log(false); 
+
+// tavola verità del NOT
+console.log("not true", !true); 
+
+console.log("not false", !false); 
+
+// tavola verità dell'AND
+console.log("true and true", true && true); 
+
+console.log("true and false", true && false);
+
+console.log("false and false", false && false); 
+
+// tavola verità dell'OR
+console.log("true or true", true || true); 
+
+console.log("true or false", true || false);
+
+console.log("false or false", false || false); 
+
+// operatore di uguaglianza 
+console.log("true === true", true === true, "true è uguale a true");
+
+console.log("false === false", false === false); 
+
+console.log("true === false", true === false, "true non è uguale a false");
+
+console.log("'pippo' === 'pippo'", 'pippo' === 'pippo'); 
+
+console.log("'pippo' === 'pluto'", "pippo" === "pluto"); 
+
+console.log("7 === 7", 7 === 7); 
+
+console.log("7 === 12", 7 === 12); 
+
+// complicazioni 
+console.log("'7' === 7", '7' === 7, "stringa diversa da numero"); 
+
+console.log("'7' == 7", '7' == 7, "vede che hanno tutti e due 7"); 
