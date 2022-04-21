605d8434769f229ed606fba135314e6c1b199faf
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-04-21 13:14:19 +0200

    ee

--- esercizioscript2.js
+++ esercizioscript2.js
@@ -1,37 +1,58 @@
 // esercizio 1
 
-// ciclo for che parta da 0 fino a 100 
-// ad ogni giro stampare il valore di i 
-//e stampare FIZZ se i divisibile per 3, BUZZ se divisibile per 5, FIZZBUZZ se divisibile per 3 e per 5 
+// ciclo for che parta da 0 fino a 100
+// ad ogni giro stampare il valore di i
+//e stampare FIZZ se i divisibile per 3, BUZZ se divisibile per 5, FIZZBUZZ se divisibile per 3 e per 5
 
-// esercizio 2 
+for (let i = 0; i < 101; i++) {
+  const isDivisibilePerCinque = i % 5 === 0;
+  const isDivisibilePerThree = i % 3 === 0;
 
-// creare programma che stampi a console questa configurazione: 
-// # # # #  
+  let result = "";
+
+if (isDivisibilePerThree && isDivisibilePerCinque) {
+        result = result + 'FIZZBUZZ'; 
+        console.log(i + ',' + result)
+    } else if (isDivisibilePerCinque){
+        result += "BUZZ"; 
+        console.log(i + ',' + result);
+    } else if (isDivisibilePerThree){
+        result += "FIZZ"; 
+        console.log(i + ',' + result)
+    } 
+else {
+      console.log(i); 
+} 
+
+}
+
+// esercizio 2
+
+// creare programma che stampi a console questa configurazione:
+// # # # #
 //  # # # #
-// # # # #  
+// # # # #
 //  # # # #
-// # # # #  
+// # # # #
 //  # # # #
-// # # # #   
+// # # # #
 
-// esercizio 3 
+// esercizio 3
 
-// creare programma che stampi a console questa configurazione: 
-// # 
-// ## 
-// ###  
-// #### 
-// ##### 
-// ###### 
+// creare programma che stampi a console questa configurazione:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
 
-// esercizio 4 
+// esercizio 4
 
-// creare programma che stampi a console questa configurazione: 
+// creare programma che stampi a console questa configurazione:
 
-//     # 
-//    ### 
-//   ##### 
-//  ####### 
+//     #
+//    ###
+//   #####
+//  #######
 // #########
-
