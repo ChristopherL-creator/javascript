6ea0325a08a570a296abfaca20ab578b48b4ec85
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-04-22 10:16:10 +0200

    W

--- esercizioscript2.js
+++ esercizioscript2.js
@@ -4,26 +4,21 @@
 // ad ogni giro stampare il valore di i
 //e stampare FIZZ se i divisibile per 3, BUZZ se divisibile per 5, FIZZBUZZ se divisibile per 3 e per 5
 
-// for (let i = 0; i < 101; i++) {
-//   const isDivisibilePerCinque = i % 5 === 0;
-//   const isDivisibilePerThree = i % 3 === 0;
+for (let i = 0; i < 101; i++) {
+  const isDivisibilePerCinque = i % 5 === 0;
+  const isDivisibilePerThree = i % 3 === 0;
 
-//   let result = "";
-
-// if (isDivisibilePerThree && isDivisibilePerCinque) {
-//         result = result + 'FIZZBUZZ'; 
-//         console.log(i + ',' + result)
-//     } else if (isDivisibilePerCinque){
-//         result += "BUZZ"; 
-//         console.log(i + ',' + result);
-//     } else if (isDivisibilePerThree){
-//         result += "FIZZ"; 
-//         console.log(i + ',' + result)
-//     } 
-// else {
-//       console.log(i); 
-// } 
-// }
+if (isDivisibilePerThree && isDivisibilePerCinque) {
+        console.log(i + ', ' + "FIZZBUZZ")
+    } else if (isDivisibilePerCinque){
+        console.log(i + ', ' + "BUZZ");
+    } else if (isDivisibilePerThree){
+        console.log(i + ', ' + "FIZZ")
+    } 
+else {
+      console.log(i); 
+} 
+}
 
 // esercizio 2
 
@@ -36,13 +31,13 @@
 //  # # # #
 // # # # # 
 
-for (let riga = 1; riga <8; riga++); {
-    // console.log("sto leggendo la riga: " + riga) 
-    for (let colonna = 1; colonna < 5; colonna++); {
-        // console.log("sto leggendo la colonna:" + colonna); 
-        console.log('sto leggendo la cella: ' + riga + ', ' + colonna);
-    }
-} 
+// for (let riga = 1; riga <8; riga++); {
+//     // console.log("sto leggendo la riga: " + riga) 
+//     for (let colonna = 1; colonna < 5; colonna++); {
+//         // console.log("sto leggendo la colonna:" + colonna); 
+//         console.log('sto leggendo la cella: ' + riga + ', ' + colonna);
+//     }
+// } 
 
 // esercizio 3
 
